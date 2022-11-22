302ce8177b0e71a5ea34030fa2c70f8ddb05ee49
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2022-11-22 12:15:52 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { BroweRouter, Routes, Route, BrowserRouter } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        } />
+        <Route path="/tes" element={<>tes</>} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
