6a9500de5c5b62a16462c3222056008bd52001f5
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2022-11-23 20:26:38 +0700

    nfts filter buttons

--- src/components/home/NFT.js
+++ src/components/home/NFT.js
@@ -1,12 +1,22 @@
 import { Col, Row } from "react-bootstrap";
-import { Title, MainCardV2 } from "../";
+import { Title, MainCardV2, Button } from "../";
 import nfts from '../../data/nfts'
 
 export default function NFT() {
+    const filterButtons = ['All', 'Art', '3D Art', 'Game', 'Painting', 'Wall Art']
     return (
         <section id="nft">
             <div className="container">
-                <Title topTitle="Trending" title="Trendings NFT's" />
+                <div className="d-flex align-items-center justify-content-between">
+                    <Title topTitle="Trending" title="Trendings NFT's" />
+                    <div className="filter">
+                        {filterButtons.map((item, index) => {
+                            return (
+                                <button key={index}>{item}</button>
+                            )
+                        })}
+                    </div>
+                </div>
                 <Row>
                     {nfts.map((item, index) => {
                         return (
@@ -14,6 +24,9 @@ export default function NFT() {
                         )
                     })}
                 </Row>
+                <div className="text-center mt-5">
+                    <Button type="link" to="/nfts" variant="outline-primary">Explore More</Button>
+                </div>
             </div>
         </section>
     )
