be6fd3517295b7b78831443bee8d3a255f3d4d05
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2022-11-24 18:45:01 +0700

    design url footer bottom

--- src/components/Footer/Bottom.js
+++ src/components/Footer/Bottom.js
@@ -4,6 +4,7 @@ export default function Bottom() {
     return (
         <Col className="bottom text-center">
             <p className="mb-0">Copyright NFT Marketplace 2022. All Right Reserved</p>
+            <small>Design By : <a href="https://www.figma.com/@madbrains">Madbrains</a></small>
         </Col>
     )
 }
